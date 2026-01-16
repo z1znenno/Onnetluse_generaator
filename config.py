41a625a98172c415838bd6ebbe4a26b403cb7600
Oleg Bereževski <116960300+z1znenno@users.noreply.github.com>
2026-01-16 12:40:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API = "c2stb3ItdjEtMTFhZDdhOTg0MzZiMDllYWQyMjUxZTk4MWQxYzU5ZGRkNjRkZDA3NzBlZTg1ZTlhNTM1NmU2MjE5YmM0ZjYzYQ=="
+API = "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"
 key = 'YWxkdyBkZmR6IGRpdWcgcWtqcQ=='
 for i in range(4):
     print(i)
